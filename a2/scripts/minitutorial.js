4876cb8a27d8a6d6a6f435244a6c889fcda951f8
Jessani <65582191+Jessani@users.noreply.github.com>
2020-09-25 00:05:18 +1000

    Add files via upload

---
+++ a2/scripts/minitutorial.js
@@ -0,0 +1,50 @@
+// hidden div class that shows content.
+function hiddenTute() {
+    var x = document.getElementById("hiddenContent");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function hiddenTute2() {
+    var x = document.getElementById("hiddenContent2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function hiddenTute3() {
+    var x = document.getElementById("hiddenContent3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function hiddenTute4() {
+    var x = document.getElementById("hiddenContent4");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function hiddenTute5() {
+    var x = document.getElementById("hiddenContent5");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  /* I wasn't sure how to simplify it.
+   another issue i encountered is that i have to double click the first time
+    i click on the boxes
+  */
