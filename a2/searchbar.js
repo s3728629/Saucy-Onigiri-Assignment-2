890d94d3a42f3b5f2744c39aa0aff3a264fc007e
s3728629 <64365826+s3728629@users.noreply.github.com>
2020-10-18 15:21:35 +1100

    Add files via upload

---
+++ a2/searchbar.js
@@ -0,0 +1,16 @@
+function showResult(str) {
+    if (str.length==0) {
+      document.getElementById("livesearch").innerHTML="";
+
+      return;
+    }
+    var xmlhttp=new XMLHttpRequest();
+    xmlhttp.onreadystatechange=function() {
+      if (this.readyState==4 && this.status==200) {
+        document.getElementById("livesearch").innerHTML=this.responseText;
+
+      }
+    }
+    xmlhttp.open("GET","livesearch.php?q="+str,true);
+    xmlhttp.send();
+  }
